2822f64237cb12184f14906812ef9ecd8a1405d5
PRAVEEN KAVINDU <crazyhacker1777@gmail.com>
2020-09-09 14:29:59 +0530

    Fuk

---
+++ userbot/plugins/Hukana Wadan.py
@@ -0,0 +1,57 @@
+from userbot.utils import admin_cmd
+
+@borg.on(admin_cmd(pattern="kne$"))
+async def gn(event):
+    await event.edit("කැරියො නැ, හාද 😏")
+    
+@borg.on(admin_cmd(pattern="deke$"))
+async def gn(event):
+    await event.edit("දෙකේ දාල පුකේ අරින්නෙ 😋")
+
+@borg.on(admin_cmd(pattern="hp$"))
+async def gn(event):
+    await event.edit("හුකහන් කැරි පකයා 😑")
+    
+@borg.on(admin_cmd(pattern="hu$"))
+async def gn(event):
+    await event.edit("හුත්ත 😑")
+    
+@borg.on(admin_cmd(pattern="huk$"))
+async def gn(event):
+    await event.edit("හුකන්න 😑")
+    
+@borg.on(admin_cmd(pattern="hp$"))
+async def gn(event):
+    await event.edit("හුකාපං 😂")
+    
+@borg.on(admin_cmd(pattern="hto$"))
+async def gn(event):
+    await event.edit("හුත්තෝහ්හ්හ්හ්හ්හ්හ්හ්හ්.....  😂")
+    
+@borg.on(admin_cmd(pattern="ponti$"))
+async def gn(event):
+    await event.edit("පොන්නයෙක් වෙන්න එපා 😂✋")
+    
+@borg.on(admin_cmd(pattern="ahut$"))
+async def gn(event):
+    await event.edit("අනේ උබ හුකාපං 😑")
+    
+@borg.on(admin_cmd(pattern="mhut$"))
+async def gn(event):
+    await event.edit("මොන හුත්තක්ද මේහ්.. 🤨")
+    
+@borg.on(admin_cmd(pattern="kw$"))
+async def gn(event):
+    await event.edit("කැරි වේසාවා...😂")
+    
+@borg.on(admin_cmd(pattern="puke$"))
+async def gn(event):
+    await event.edit("අකේපුරන්නව දැන ගනින් 😂🤨")
+    
+@borg.on(admin_cmd(pattern="idpn$"))
+async def gn(event):
+    await event.edit("පොඩ්ඩක් ඉදපන් දෙන්නම් 😁✋")
+    
+@borg.on(admin_cmd(pattern="mw$"))
+async def gn(event):
+    await event.edit("මැරියන් වේසාවා 😑😂")
